399e39386085a25d4f1045efb3869e686d21905e
Med Bouya <medbouya88@hotmail.com>
2026-02-27 01:44:30 +0000

    fix: Use correct DATABASE_URI environment variable

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -42,7 +42,7 @@ export default buildConfig({
   },
   db: postgresAdapter({
     pool: {
-      connectionString: process.env.DATABASE_URL || '',
+      connectionString: process.env.DATABASE_URI || '',
     },
     migrationDir: path.resolve(dirname, 'migrations'),
   }),
